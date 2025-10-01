a2046c5363a26e0c33762e0510052c51f4d0830c
Afrah-F <s202257660@kfupm.edu.sa>
2025-10-01 13:52:41 +0300

    5-2 is completed

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
-
+import StudentCard from './components/StudentCard.jsx'
 function App() {
-  return (
-    <div className="app">
-      <header className="dashboard-header">
-        <h1>Student Information Dashboard</h1>
-        <p>View and manage student details</p>
-      </header>
-
-      <main className="dashboard-main">
-        <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+    return (
+        <div className="app">
+            <header className="dashboard-header">
+                <h1>Student Information Dashboard</h1>
+                <p>View and manage student details</p>
+            </header>
+            <main className="dashboard-main">
+                <div className="cards-container">
+                    <StudentCard name="Afrah" id="s202257660" dept="Computer Science" />
+                    <StudentCard name="Maryam Alzahrani" id="s2022xxxx" dept="Finance" />
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  )
+    )
 }
-
 export default App
